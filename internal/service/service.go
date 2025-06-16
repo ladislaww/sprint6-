f8175d149c7c58c4ea2d1ad8b086667d513cde1f
Vlad Sivachev <linudaisb@gmai.com>
2025-06-16 22:34:35 +0300

    Закончил функцию определения морзе

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"os"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func FileContent(path string) (string, error) {
+	content, err := os.ReadFile(path)
+
+	if err != nil {
+		return "", err
+	}
+
+	var result string
+	text := string(content)
+
+	if strings.ContainsAny(strings.ToLower(text), "абвгдеёжзийклмнопрстуфхцчшщъыьэюя") {
+		result = morse.ToMorse(text)
+
+	} else {
+		result = morse.ToText(text)
+	}
+
+	if err := os.WriteFile(path, []byte(result), 0755); err != nil {
+		return "", err
+	}
+	return path, nil
+}
